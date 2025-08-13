e10240b3b3f0bce800dbf82c3b5c43c796529642
Sharon Lee <ssmlee10@gmail.com>
2025-08-13 12:29:39 -0700

    create router skeleton

---
+++ controllers/jukebox.js
@@ -0,0 +1,15 @@
+const Jukebox = require('../models/jukebox.js');
+const express = require('express');
+const router = express.Router();
+
+// POST - create a track
+
+// GET - index - list all tracks
+
+// GET - show - get a single track
+
+// PUT - update - update a track
+
+// DELETE - delete a track
+
+module.exports = router;
