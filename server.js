3ee7d00eb86f1cf4ff4a5ce2961674aabdac6f36
Sharon Lee <ssmlee10@gmail.com>
2025-08-13 12:21:51 -0700

    install cors, install all packages, create jukebox controller

---
+++ server.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors');
+
+const jukeboxRouter = require('./controllers/jukebox.js');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+app.listen(3000, () => {
+    console.log('The express app is ready!');
+});
